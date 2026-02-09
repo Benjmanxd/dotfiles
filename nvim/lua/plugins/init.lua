4ec96560214dc67677ab01c43145e2cf83c4e72d
Benjmanxd <benjaminchoi00@gmail.com>
2026-02-09 16:35:22 +0800

    nvim: misc changes

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,5 +1,6 @@
 return {
 	{ "norcalli/nvim-colorizer.lua", opts = { "*" } }, -- color
 	{ "numToStr/Comment.nvim", opts = {} }, -- comments
-	"tpope/vim-fugitive", -- git commands
+	{ "tpope/vim-fugitive" }, -- git commands
+	{ "ThePrimeagen/vim-be-good" }, -- little vim motion game
 }
